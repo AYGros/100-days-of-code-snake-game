4186437d7fe70ab817b410bfd79dd37c68c972cc
yuki <mansie.torsholt@gmail.com>
2021-09-21 22:53:29 +0200

    create anddetect food and move food to new location

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.title("Yuki's snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 
@@ -36,6 +38,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    #detect collistion with food using distance method from Turtle
+    if snake.head.distance(food) < 12:
+        food.refresh()
 
 
 
--- snake.py
+++ snake.py
@@ -1,11 +1,13 @@
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-MOVE_DISTANCE =20
+MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
+
 class Snake:
     def __init__(self):
 
@@ -23,7 +25,7 @@ class Snake:
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x =self.segments[seg_num - 1].xcor()
+            new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
@@ -43,4 +45,3 @@ class Snake:
     def right(self):
         if self.head.heading != LEFT:
             self.head.setheading(RIGHT)
-
